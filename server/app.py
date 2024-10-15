aaa7c7b538958529d8a5bcb7daf91e8742192170
Anne Liza Wangui <annewangui210@gmail.com>
2024-10-15 10:36:43 +0300

    Changes app configuration

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ from db import db
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 db.init_app(app)
 migrate = Migrate(app, db)
